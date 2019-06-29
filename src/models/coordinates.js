13d9b783db5da7d398933302c04c04ae385cd3f6
Cactice <14835424+Cactice@users.noreply.github.com>
2019-06-30 06:37:19 +0900

    Add assets

---
+++ src/models/coordinates.js
@@ -0,0 +1,10 @@
+export default class Coordinates{
+    constructor(latitude, longitude){
+      this.longitude = longitude
+      this.latitude = latitude
+    //   this.state = {
+    //     slider1ActiveSlide: SLIDER_1_FIRST_ITEM
+    // };  
+    // I don't know what this is
+    }
+  }
