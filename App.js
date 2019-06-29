64586d1182a1775928766ebbfbcd70882f4984e0
Cactice <14835424+Cactice@users.noreply.github.com>
2019-06-30 06:24:47 +0900

    Add a lot of things

--- App.js
+++ App.js
@@ -1,62 +1,158 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native'
+
+import {
+  createSwitchNavigator,
+  createStackNavigator ,
+  createDrawerNavigator,
+  createMaterialTopTabNavigator
+} from 'react-navigation'
+
 import { Ionicons } from '@expo/vector-icons';
 
-import AppNavigator from './navigation/AppNavigator';
+// Auth stack screen imports
+import AuthLoadingScreen from './src/screens/AuthLoadingScreen'
+import WelcomeScreen from './src/screens/WelcomeScreen'
+import SignUpScreen from './src/screens/SignUpScreen'
+import SignInScreen from './src/screens/SignInScreen'
+import ForgetPasswordScreen from './src/screens/ForgetPasswordScreen'
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+// App stack screen imports
+import HomeScreen from './src/screens/HomeScreen'
+import SettingsScreen from './src/screens/SettingsScreen'
+import ProfileScreen from './src/screens/ProfileScreen'
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
+// Amplify imports and config
+import Amplify from '@aws-amplify/core'
+import config from './src/aws-exports'
+Amplify.configure(config)
+
+// Configurations and options for the AppTabNavigator
+const configurations = {
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons style={{fontSize: 26, color: tintColor}} name="ios-home" />
+      )
+    }
+  },
+  Profile: {
+    screen: ProfileScreen,
+    navigationOptions: {
+      tabBarLabel: 'Profile',
+      tabBarIcon: ({tintColor}) => (
+        <Ionicons style={{fontSize: 26, color: tintColor}} name="ios-person" />
+      )
+    }
+  },
+  Settings: {
+    screen: SettingsScreen,
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons style={{fontSize: 26, color: tintColor}} name="ios-settings" />
+      )
+    }
+  },
+}
+
+const options = {
+  tabBarPosition: 'bottom',
+  swipeEnabled: true,
+  animationEnabled: true,
+  navigationOptions: {
+    tabBarVisible: true
+  },
+  tabBarOptions: {
+    showLabel: true,
+    activeTintColor: '#fff',
+    inactiveTintColor: '#fff9',
+    style: {
+      backgroundColor: '#f16f69',
+    },
+    labelStyle: {
+      fontSize: 12,
+      fontWeight: 'bold',
+      marginBottom: 12,
+      marginTop:12,
+    },
+    indicatorStyle: {
+      height: 0,
+    },
+    showIcon: true,
   }
 }
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+// Bottom App tabs
+const AppTabNavigator = createMaterialTopTabNavigator(configurations, options)
+
+// Making the common header title dynamic in AppTabNavigator
+AppTabNavigator.navigationOptions = ({ navigation }) => {
+  let { routeName } = navigation.state.routes[navigation.state.index]
+  let headerTitle = routeName
+  return {
+    headerTitle,
+  }
+}
+
+const AppStackNavigator = createStackNavigator({
+  Header: {
+    screen: AppTabNavigator,
+    // Set the header icon
+    navigationOptions: ({navigation}) => ({
+      headerLeft: (
+        <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+          <View style={{paddingHorizontal: 10}}>
+            <Ionicons size={24} name="md-menu" />
+          </View>
+        </TouchableOpacity>
+      )
+    })
+  }
+})
+
+// App stack for the drawer
+const AppDrawerNavigator = createDrawerNavigator({
+  Tabs: AppStackNavigator, // defined above
+  Home: HomeScreen,
+  Profile: ProfileScreen,
+  Settings: SettingsScreen
+})
+
+// Auth stack
+const AuthStackNavigator = createStackNavigator({
+  Welcome: {
+    screen: WelcomeScreen,
+    navigationOptions: () => ({
+      title: `Welcome to this App`, // for the header screen
+      headerBackTitle: 'Back'
     }),
-  ]);
-}
-
-function handleLoadingError(error: Error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
   },
-});
+  SignUp: {
+    screen: SignUpScreen,
+    navigationOptions: () => ({
+      title: `Create a new account`,
+    }),
+  },
+  SignIn: {
+    screen: SignInScreen,
+    navigationOptions: () => ({
+      title: `Log in to your account`,
+    }),
+  },
+  ForgetPassword: {
+    screen: ForgetPasswordScreen,
+    navigationOptions: () => ({
+      title: `Create a new password`,
+    }),
+  },
+})
+
+export default createSwitchNavigator({
+  Authloading: AuthLoadingScreen,
+  Auth: AuthStackNavigator, // the Auth stack
+  App: AppDrawerNavigator, // the App stack
+})
+
